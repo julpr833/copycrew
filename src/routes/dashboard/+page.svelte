879a503958d8f9d53b475c3b955d7b2f18100dd1
leScythekill <julianpretto16@gmail.com>
2024-03-22 10:45:07 -0300

    add dashboard

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,99 @@
+<!-- dashboard -->
+<section id="dashboard" class="h-[92.6vh]">
+	<div class="flex flex-col w-full items-center">
+		<h2 class="text-black dark:text-white font-bold text-3xl mt-12">
+			Welcome, <span class="text-purple-500">username</span>, what do you want to do today?
+		</h2>
+		<div class="grid grid-cols-2 grid-rows-2 gap-4 text-black dark:text-white mt-12">
+			<div
+				class="bg-slate-900 p-8 rounded-lg cursor-pointer transition-transform hover:scale-105 duration-300"
+			>
+				<div class="flex gap-1 items-center text-purple-400 stroke-purple-400">
+					<svg
+						width="32"
+						height="32"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path
+							d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"
+						/>
+						<path
+							d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"
+						/>
+					</svg>
+					<h3 class="text-xl font-bold font-sans">My copypastes</h3>
+				</div>
+				<p class="mt-4">View and edit your personal copypastes</p>
+			</div>
+			<div
+				class="bg-slate-900 p-8 rounded-lg cursor-pointer transition-transform hover:scale-105 duration-300"
+			>
+				<div class="flex gap-1 items-center text-purple-400 stroke-purple-400">
+					<svg
+						width="32"
+						height="32"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+						<path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
+						<path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+						<path d="M17 10h2a2 2 0 0 1 2 2v1" />
+						<path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+						<path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
+					</svg>
+					<h3 class="text-xl font-bold font-sans">My groups</h3>
+				</div>
+				<p class="mt-4">View the copypastes posted in the groups you have joined</p>
+			</div>
+			<div
+				class="bg-slate-900 p-8 rounded-lg cursor-pointer transition-transform hover:scale-105 duration-300"
+			>
+				<div class="flex gap-1 items-center text-purple-400 stroke-purple-400">
+					<svg
+						width="32"
+						height="32"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path
+							d="M9 3h-4a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h4a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2z"
+							stroke-width="0"
+							fill="currentColor"
+						/>
+						<path
+							d="M9 13h-4a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h4a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2z"
+							stroke-width="0"
+							fill="currentColor"
+						/>
+						<path
+							d="M19 13h-4a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h4a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2z"
+							stroke-width="0"
+							fill="currentColor"
+						/>
+						<path
+							d="M17 3a1 1 0 0 1 .993 .883l.007 .117v2h2a1 1 0 0 1 .117 1.993l-.117 .007h-2v2a1 1 0 0 1 -1.993 .117l-.007 -.117v-2h-2a1 1 0 0 1 -.117 -1.993l.117 -.007h2v-2a1 1 0 0 1 1 -1z"
+							stroke-width="0"
+							fill="currentColor"
+						/>
+					</svg>
+					<h3 class="text-xl font-bold font-sans">Create group</h3>
+				</div>
+				<p class="mt-4">Create a group to share copypastes between your friends</p>
+			</div>
+		</div>
+	</div>
+</section>
