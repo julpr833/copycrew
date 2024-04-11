367f3b9e64729feed9a6cec35c9a8aaa5162cbd9
leScythekill <julianpretto16@gmail.com>
2024-04-11 18:11:03 -0300

    move navbar component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,48 @@
 <script lang="ts">
-	import Navbar from '../components/Navbar.svelte';
+	import NavbarLogo from '../components/NavbarLogo.svelte';
+	import NavbarDropdown from '../components/NavbarDropdown.svelte';
+	import NavbarMobileMenu from '../components/NavbarMobileMenu.svelte';
 	import '../app.css';
+
+	export let data;
 </script>
 
-<Navbar />
-<main class="main-background">
+<nav
+	class="dark:text-white dark:bg-gradient-to-r from-[#252435] to-[#131f31] p-3 flex justify-center"
+>
+	<div class="flex flex-row items-center justify-between xl:min-w-[60%] md:min-w-full">
+		<NavbarLogo />
+
+		<div class="hidden md:flex flex-row gap-16">
+			<ul class="flex flex-row gap-3"></ul>
+
+			{#if data.user}
+				<NavbarDropdown />
+			{/if}
+
+			{#if !data.user}
+				<ul class="flex flex-row gap-4">
+					<li>
+						<a
+							href="/login"
+							class="hover:opacity-80 transition-opacity duration-300 hover:text-[#583d7c] dark:hover:text-[#cdaff5]"
+							>Login</a
+						>
+					</li>
+					<li>
+						<a
+							href="/signup"
+							class=" border border-black dark:border-white rounded-md py-2 px-4 hover:bg-black dark:hover:bg-white transition-colors duration-300 hover:text-white dark:hover:text-black"
+							>Sign up</a
+						>
+					</li>
+				</ul>
+			{/if}
+		</div>
+		<NavbarMobileMenu user={data.user} />
+	</div>
+</nav>
+
+<main>
 	<slot />
 </main>
-
-<style>
-	/* Dark scheme background */
-
-	@media (prefers-color-scheme: dark) {
-		.main-background {
-			background: radial-gradient(
-				circle at center,
-				#242435 0%,
-				#212145 50%,
-				#1d2233 60%,
-				#171731 100%
-			);
-			background-size: 400% 400%;
-			animation: 17s ease infinite backgroundAnimation;
-		}
-	}
-
-	@keyframes backgroundAnimation {
-		0% {
-			background-position: 0% 50%;
-		}
-
-		50% {
-			background-position: 100% 50%;
-		}
-
-		100% {
-			background-position: 0% 50%;
-		}
-	}
-</style>
