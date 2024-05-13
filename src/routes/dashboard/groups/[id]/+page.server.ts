ee462c1cc4ea53b7b8112e20c0660cadceac4893
leScythekill <julianpretto16@gmail.com>
2024-05-13 16:22:50 -0300

    fix > changed order in which copypastes are loaded in group page.

--- src/routes/dashboard/groups/[id]/+page.server.ts
+++ src/routes/dashboard/groups/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import db from '$lib/database.js';
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export async function load({ params, locals }) {
 	const userId = locals.user.id;
@@ -24,6 +24,9 @@ export async function load({ params, locals }) {
 							username: true
 						}
 					}
+				},
+				orderBy: {
+					id: 'desc'
 				}
 			}
 		}
@@ -33,7 +36,46 @@ export async function load({ params, locals }) {
 		throw redirect(303, '/dashboard/groups');
 
 	return {
-		group: group.name,
+		group: { name: group.name, id: group.id },
 		copypastes: group.copypastes
 	};
 }
+
+export const actions = {
+	add: async ({ request, locals }) => {
+		const data = await request.formData();
+		const { group, title, content, categories: unparsedCategories } = Object.fromEntries(data);
+		const categories = JSON.parse(unparsedCategories as string);
+
+		if (!title || !content) {
+			return error(400, { message: `${!title ? 'Title' : 'Content'} is required` });
+		}
+
+		if (title.toString().length < 4 || title.toString().length > 32) {
+			return error(400, { message: 'Title must be between 4 and 32 characters long' });
+		}
+
+		if (categories.length < 1 || !categories) {
+			return error(400, { message: 'At least one category is required' });
+		}
+
+		if (categories.some((category: string) => category.includes(' '))) {
+			return error(400, { message: 'Categories cannot contain spaces' });
+		}
+
+		try {
+			const newCopypaste = await db.copypaste.create({
+				data: {
+					title: title.toString(),
+					content: content.toString(),
+					author_id: locals.user.id,
+					group_id: +group,
+					categories
+				}
+			});
+			return newCopypaste;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+};
