51c881037dbc7c6e1d03c3b05351ace897ca29d8
leScythekill <julianpretto16@gmail.com>
2024-05-15 12:40:06 -0300

    minor style fixes

--- src/routes/dashboard/groups/[id]/components/Add.svelte
+++ src/routes/dashboard/groups/[id]/components/Add.svelte
@@ -230,40 +230,12 @@
 	.spin {
 		animation: spin 1s infinite;
 	}
-
-	:global(.shake-error) {
-		animation: shake 0.82s infinite cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
-		transform: translate3d(0, 0, 0);
-		transition-duration: 100ms;
-		background-color: #bd3636 !important;
-	}
-
 	@keyframes spin {
 		0% {
 			transform: rotate(0deg);
 		}
 		100% {
 			transform: rotate(360deg);
-		}
-	}
-
-	@keyframes shake {
-		10%,
-		90% {
-			transform: translate3d(-1px, 0, 0);
-		}
-		20%,
-		80% {
-			transform: translate3d(2px, 0, 0);
-		}
-		30%,
-		50%,
-		70% {
-			transform: translate3d(-4px, 0, 0);
-		}
-		40%,
-		60% {
-			transform: translate3d(4px, 0, 0);
 		}
 	}
 </style>
